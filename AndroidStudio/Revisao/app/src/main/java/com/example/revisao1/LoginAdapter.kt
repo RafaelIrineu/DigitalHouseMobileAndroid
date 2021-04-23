125b49e1b96d3ec1035411e33b8a41b761f419e3
RafaelIrineu <rafaelirineu01@gmail.com>
2021-04-23 12:04:56 -0300

    Revisao - melhorando experiencia do usuário

--- AndroidStudio/Revisao/app/src/main/java/com/example/revisao1/LoginAdapter.kt
+++ AndroidStudio/Revisao/app/src/main/java/com/example/revisao1/LoginAdapter.kt
@@ -5,10 +5,11 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
 class LoginAdapter(private val frags: List<Fragment>,
-    //lista de fragments
+                    //lista de fragments
                    private val titles: List<String>,
-    //lista de títulos
+                    //lista de títulos
                    manager: FragmentManager): FragmentPagerAdapter(manager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+                    //receita
     override fun getCount() = frags.size
     //retorna tamanho da lista de fragments, ou seja, qtd de tabs
     override fun getItem(position: Int) = frags[position]
--- AndroidStudio/Revisao/app/src/main/java/com/example/revisao1/LoginFragment.kt
+++ AndroidStudio/Revisao/app/src/main/java/com/example/revisao1/LoginFragment.kt
@@ -1,17 +1,83 @@
 package com.example.revisao1
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import org.w3c.dom.Text
 
 class LoginFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        //primeiro de tudo associar a view! não esquecer do return no fim e tirar o return padrão!!
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        val btnLogin = view.findViewById<MaterialButton>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            if (validaEntradas(view)) {
+                Toast.makeText(view.context, "Login realizado", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        //abaixo o código para fazer com que após dar o erro de campo vazio, ao começar a digitar
+        //o erro seja escondido
+        view.findViewById<TextInputEditText>(R.id.editTextUserNameLogin)
+            .addTextChangedListener(object :
+                TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
+                }
+
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    view.findViewById<TextInputLayout>(R.id.txtUserNameLogin).error = ""
+                }
+            })
+
+        view.findViewById<TextInputEditText>(R.id.editTextPasswordLogin)
+            .addTextChangedListener(object :
+                TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
+                }
+
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    view.findViewById<TextInputLayout>(R.id.txtPasswordLogin).error = ""
+                }
+            })
+
+        return view
+    }
+
+    fun validaEntradas(view: View): Boolean {
+        var resultado = true
+
+        val edtUserName = view.findViewById<TextInputEditText>(R.id.editTextUserNameLogin)
+        val edtPassword = view.findViewById<TextInputEditText>(R.id.editTextPasswordLogin)
+
+        if (edtUserName.text?.trim()!!.isBlank()) {
+            view.findViewById<TextInputLayout>(R.id.txtUserNameLogin).error = "Usuário vazio"
+            resultado = false
+        }
+
+        if (edtPassword.text?.trim()!!.isBlank()) {
+            view.findViewById<TextInputLayout>(R.id.txtPasswordLogin).error = "Senha vazia"
+            resultado = false
+        }
+
+        return resultado
     }
 }
